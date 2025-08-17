66f19b973aae483dc1a6e095b5a3377aaf7b6f94
mahmoudashraf0011 <ma7157563@gmail.com>
2025-08-17 21:03:58 +0300

    first commit

--- .history/src/App_20250817210127.tsx
+++ .history/src/App_20250817210127.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
@@ -15,16 +14,17 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/Portfolio">
+      <BrowserRouter basename="/Portfolio">
           <Routes>
             <Route path="/" element={<Index />} />
-            {/* ضع كل الـ Routes المخصصة هنا */}
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </React.StrictMode>
+
 );
 
 export default App;
